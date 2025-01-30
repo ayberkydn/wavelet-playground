a375bbe7ea7863ab2a61a50cbd57ad52ce1b8007
installer <installer@gmail.com>
2025-01-30 19:23:37 +0300

    upd

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from skimage import data
-from utils import compute_dwt, normalize, shift_rotate_img, coeff_to_img, plot_histogram, reconstruct_dwt
+from utils import compute_dwt, scale, shift_rotate_img, coeff_to_img, plot_histogram, reconstruct_dwt
 from skimage.transform import resize
 import pywt
 
@@ -8,10 +8,10 @@ import pywt
 
 def main():
     st.set_page_config(
-        page_title="Wavelet Playground - Resized Frames for Video",
+        page_title="Wavelet Playground ",
         layout="wide"
     )
-    st.title("Wavelet Playground with Resized Video Frames")
+    st.title("Wavelet Playground")
 
     # ----------------- Session State for shifting/rotating -------------
     if "offset_x" not in st.session_state:
@@ -36,14 +36,14 @@ def main():
         }
         selected_sample = st.selectbox("Select a Sample Image", list(sample_images.keys()))
         
-        wavelets = ['haar', 'db2', 'db3']
+        wavelets = ['haar', 'db2', 'db3', 'coif1', 'coif2', 'coif3', 'sym2', 'sym3']
         selected_wavelet = st.selectbox("Select a Wavelet", wavelets, index=0)
      
         # levels = st.slider("DWT Levels", min_value=1, max_value=8, value=8)
 
         # SHIFT CONTROLS
-        st.markdown("### Shift the image")
-        shift_step = st.slider("Shift step (pixels)", 0.5, 2.0, 0.5)
+        st.markdown("### Shift")
+        shift_step = st.radio("Shift step (pixels)", [0.1, 0.5, 1, 5], index=0, horizontal=True)
         row_up = st.columns([1, 1, 1], gap="small")
         row_mid = st.columns([1, 1, 1], gap="small")
         row_down = st.columns([1, 1, 1], gap="small")
@@ -82,30 +82,24 @@ def main():
                 st.session_state["offset_x"] += shift_step
 
         # ROTATION CONTROLS
-        st.markdown("### Rotate the image")
-        rotation_row = st.columns([1, 1, 1, 1, 1, 1, 1], gap="small")
+        st.markdown("### Rotate")
+        rotation_row = st.columns([1, 1, 1, 1, 1], gap="small")
         with rotation_row[0]:
-            if st.button("<-", key="rotate_left1"):
-                st.session_state["rotation_angle"] += 30
-        with rotation_row[1]:
-            if st.button("<-", key="rotate_left2"):
+            if st.button("-5°", key="rotate_left2"):
                 st.session_state["rotation_angle"] += 5
-        with rotation_row[2]:
-            if st.button("<-", key="rotate_left3"):
+        with rotation_row[1]:
+            if st.button("-1°", key="rotate_left3"):
                 st.session_state["rotation_angle"] += 1
-        with rotation_row[3]:
+        with rotation_row[2]:
             if st.button("0", key="reset_rotation"):
                 st.session_state["rotation_angle"] = 0
-        with rotation_row[4]:
-            if st.button("->", key="rotate_right1"):
+        with rotation_row[3]:
+            if st.button("1°", key="rotate_right1"):
                 st.session_state["rotation_angle"] -= 1
-        with rotation_row[5]:
-            if st.button("->", key="rotate_right2"):
+        with rotation_row[4]:
+            if st.button("5°", key="rotate_right2"):
                 st.session_state["rotation_angle"] -= 5
-        with rotation_row[6]:
-            if st.button("->", key="rotate_right3"):
-                st.session_state["rotation_angle"] -= 50
-
+        
         st.markdown(
             f"**X** = {st.session_state['offset_x']:.2f} | "
             f"**Y** = {st.session_state['offset_y']:.2f}"
@@ -123,57 +117,53 @@ def main():
         shift_y=st.session_state["offset_y"],
         angle=st.session_state["rotation_angle"]
     )
-    base_img = normalize(base_img)  # scale to [0,1]
+    base_img = scale(base_img, min_val=-1, max_val=1)
 
     # Wavelet decomposition
-    wavelet_img, coeffs = compute_dwt(
+    wavelet_img, coeffs, cHs, cVs, cDs = compute_dwt(
         base_img,
         wavelet=selected_wavelet
     )
 
-    levels = len(coeffs) - 1
-
 
     # --------------- Display in Tabs --------------
     tab1, tab2, tab3, tab4 = st.tabs([
-        "Transformed Image", 
+        "Image", 
         "Wavelet Decomposition", 
         "Wavelet Coefficients",
         "Reconstruction"
     ])
 
     with tab1:
-        st.image(base_img, caption="Shifted + Rotated Image", use_container_width=True)
+        st.image(scale(base_img, min_val=0, max_val=1), caption="Image", use_container_width=True)
 
     with tab2:
-        st.image(wavelet_img, caption="Wavelet Decomposition (approx = 0)", use_container_width=True)
+        st.image(scale(wavelet_img, min_val=0, max_val=1), caption="Wavelet Decomposition (approx = 0)", use_container_width=True)
 
     with tab3:
-        slider_coeff = st.slider("Level for Coefficients", min_value=1, max_value=levels, value=1, key="coeff_slider")
+        slider_coeff = st.slider("Level for Coefficients", min_value=1, max_value=len(coeffs)-1, value=1, key="coeff_slider")
         r1, r2, r3 = st.columns([1, 1, 1], gap="small")
         with r1:
-            st.image(coeff_to_img(coeffs[slider_coeff][0], (512, 512)), caption="Horizontal Coefficients", use_container_width=True)
-            st.plotly_chart(plot_histogram(coeffs[slider_coeff][0], "Horizontal Coefficients Histogram"))
+            st.image(coeff_to_img(cHs[slider_coeff-1], (512, 512)), caption="Horizontal Coefficients", use_container_width=True)
+            st.plotly_chart(plot_histogram(cHs[slider_coeff-1], title=""))
         with r2:
-            st.image(coeff_to_img(coeffs[slider_coeff][1], (512, 512)), caption="Vertical Coefficients", use_container_width=True)
-            st.plotly_chart(plot_histogram(coeffs[slider_coeff][1], "Vertical Coefficients Histogram"))
+            st.image(coeff_to_img(cVs[slider_coeff-1], (512, 512)), caption="Vertical Coefficients", use_container_width=True)
+            st.plotly_chart(plot_histogram(cVs[slider_coeff-1], title=""))
         with r3:
-            st.image(coeff_to_img(coeffs[slider_coeff][2], (512, 512)), caption="Diagonal Coefficients", use_container_width=True)
-            st.plotly_chart(plot_histogram(coeffs[slider_coeff][2], "Diagonal Coefficients Histogram"))
+            st.image(coeff_to_img(cDs[slider_coeff-1], (512, 512)), caption="Diagonal Coefficients", use_container_width=True)
+            st.plotly_chart(plot_histogram(cDs[slider_coeff-1], title=""))
 
     with tab4:
         # Add a segmented control for selecting DWT levels
         levels_list = list(range(1, len(coeffs)))
-        selected_levels = st.segmented_control(
+        removed_levels = st.segmented_control(
             "Removed DWT Levels",
             options=levels_list,
             selection_mode="multi",
             default=[],
         )
-        reconstruct_img = reconstruct_dwt(coeffs, selected_levels, selected_wavelet)
-        st.write(reconstruct_img.max())
-        st.write(reconstruct_img.min())
-        st.image(reconstruct_img, caption="Reconstructed Image", use_container_width=True, clamp=True)
+        reconstruct_img = scale(reconstruct_dwt(coeffs, removed_levels, selected_wavelet), min_val=0, max_val=1)
+        st.image(reconstruct_img, caption="Reconstructed Image", use_container_width=True)
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -10,16 +10,15 @@ from scipy.ndimage import shift
 import plotly.express as px
 import streamlit as st
 
-def normalize(x):
-    """Linearly scales array x to [0..1] unless x is constant."""
+def scale(x, min_val, max_val):
     if np.allclose(x.max(), x.min()):
         return x
     else:
         x = (x - x.min()) / (x.max() - x.min())
+        x = x * (max_val - min_val) + min_val
     return x
 
 def shift_rotate_img(img, shift_x, shift_y, angle):
-    """Shifts and rotates an image."""
     img = shift(img, (shift_y, shift_x), mode='nearest')
     img = rotate(img, angle, resize=False, mode='symmetric')
     return img
@@ -31,11 +30,17 @@ def coeff_to_img(coeff, size, order=0):
 
 def compute_dwt(img, wavelet):
     coeffs = pywt.wavedec2(img, wavelet=wavelet)
-    # coeffs = pywt.wavedec2(img, wavelet=wavelet, level=levels)
     wavelet_img, _ = pywt.coeffs_to_array(coeffs)
-    wavelet_img = np.abs(np.clip(wavelet_img, -1, 1))
+    # cAs = []
+    # for i in range(1, len(coeffs)):
+    #     removed_levels = [n for n in range(len(coeffs)) if n >= i]
+    #     cAs.append(reconstruct_dwt(coeffs, removed_levels, wavelet))
 
-    return wavelet_img, coeffs
+    cHs = [coeff[0] for coeff in coeffs[1:]]
+    cVs = [coeff[1] for coeff in coeffs[1:]]
+    cDs = [coeff[2] for coeff in coeffs[1:]]
+
+    return wavelet_img, coeffs, cHs, cVs, cDs
 
 def reconstruct_dwt(coeffs, removed_levels, wavelet):
     for level in removed_levels:
@@ -51,7 +56,7 @@ def plot_histogram(coeff, title):
     """Plots a histogram of the given coefficients."""
     fig = px.histogram(
         x=coeff.flatten(),
-        nbins=255,
+        nbins=16,
         title=title,
         labels={'x': 'Coefficient Value', 'y': 'Frequency'}
     )
